15fa5fe46cb2f39456ba94f356161c444488cb65
Pxr <Pxr@DESKTOP-3P4EVOM>
2024-08-24 16:08:26 +0300

    category and product controller done some icon updates

---
+++ Foody.PresentationLayer/Controllers/CategoriesController.cs
@@ -0,0 +1,53 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Foody.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _categoryService.TGetAll();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category category) 
+        {
+            _categoryService.TInsert(category);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            _categoryService.TDelete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var value = _categoryService.TGetById(id);
+            return View(value);
+        }
+        public IActionResult UpdateCategory(Category category)
+        {
+            _categoryService.TUpdate(category);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Foody.PresentationLayer/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Foody.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Foody.PresentationLayer.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public ProductsController(IProductService productService, ICategoryService categoryService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _productService.TProductListWithCategory();
+            return View(values);
+        }
+
+        public IActionResult DeleteProduct(int id)
+        {
+            _productService.TDelete(id);
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            var values = _categoryService.TGetAll();
+            ViewBag.categories = new SelectList(values, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            _productService.TInsert(product);
+            return RedirectToAction("ProductListWithCategory");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateProduct(int id)
+        {
+            var values = _categoryService.TGetAll();
+            ViewBag.categories = new SelectList(values, "CategoryId", "CategoryName");
+            var productValues = _productService.TGetById(id);
+            return View(productValues);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProduct(Product product)
+        {
+            _productService.TUpdate(product);
+            return RedirectToAction("ProductListWithCategory");
+        }
+
+
+
+    }
+}
